7f69b7877180caed37ce4d207bc22e57658d32ea
reddavis <reddavis@gmail.com>
2010-10-05 11:03:29 +0100

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :http_authenticate if Rails.env == "production"
+
+  private
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |login, password|
+      login == LOGIN && password == PASSWORD
+    end
+  end
 end
